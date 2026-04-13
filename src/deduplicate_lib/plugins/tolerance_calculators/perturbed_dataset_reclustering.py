eca4ab05c575b766eb0af2747c82de6ac4d18469
Julian <holland@fhi.mpg.de>
2026-04-13 11:58:21 +0200

    tight and loose options to pdr with tes

--- src/deduplicate_lib/plugins/tolerance_calculators/perturbed_dataset_reclustering.py
+++ src/deduplicate_lib/plugins/tolerance_calculators/perturbed_dataset_reclustering.py
@@ -6,13 +6,33 @@ import numpy as np
 
 @register_plugin("tolerance_calculator", "perturbed_dataset_reclustering")
 class PerturbedDatasetReclustering(ToleranceCalculator):
+    """A tolerance calculator that creates a perturbed dataset from the original dataset, then uses the duplicate detection algorithm to find the tolerance that yields the same number of unique structures in the perturbed dataset as in the target. The tolerance can be calculated as 'loose' (the highest tolerance that yields the target number of unique structures), 'tight' (the lowest tolerance that yields the target number of unique structures), or 'average' (the average of the highest and lowest tolerances that yield the target number of unique structures).
+
+    Args:
+        duplicate_detection_algorithm_object (DuplicateDetectionAlgorithm): The duplicate detection algorithm object to use for calculating the tolerance.
+        tolerance_dataset_array (np.ndarray, optional): The dataset array to use for calculating the tolerance. If not provided, the dataset array from the duplicate detection algorithm object will be used. Defaults to np.array([]).
+        perturbations_per_vector (int, optional): The number of perturbations to create for each vector in the dataset array. Defaults to 1.
+        perturbation_scale (float, optional): The scale of the perturbations to create. Defaults to 0.1.
+        binary_search_steps (int, optional): The number of steps to use for the binary search to find the tolerance. Defaults to 20.
+        target_structures (int | None, optional): The target number of unique structures to find the tolerance for. If None, the target will be the number of unique structures in the original dataset array. Defaults to None.
+        target_structures_threshold (str, optional): The method to use for calculating the tolerance threshold. Can be 'average', 'loose', or 'tight'. Defaults to 'average'.
+
+    Functions:
+        calculate_tolerance: Calculate the tolerance value based on the perturbed dataset and the target number of unique structures.
+
+        Returns:
+            float: The calculated tolerance value.
+    """
     def __init__(self,
         duplicate_detection_algorithm_object: DuplicateDetectionAlgorithm,
         tolerance_dataset_array: np.ndarray = np.array([]),
         perturbations_per_vector: int = 1,
         perturbation_scale: float = 0.1,
         binary_search_steps: int = 20,
-        target_structures: int | None = None,):
+        target_structures: int | None = None,
+        target_structures_threshold: str = "average"
+    ):
+
         super().__init__(
             duplicate_detection_algorithm_object=duplicate_detection_algorithm_object,
             tolerance_dataset_array=tolerance_dataset_array,
@@ -23,7 +43,7 @@ class PerturbedDatasetReclustering(ToleranceCalculator):
         if target_structures is None:
             target_structures = len(duplicate_detection_algorithm_object.dataset_array)
         self.target_structures = target_structures
-
+        self.target_structures_threshold = target_structures_threshold
     def __str__(self) -> str:
         return f"PerturbedDatasetReclustering(perturbations_per_vector={self.perturbations_per_vector}, perturbation_scale={self.perturbation_scale}, dda={str(self.duplicate_detection_algorithm_object).split('(')[0]})"
     
@@ -34,13 +54,24 @@ class PerturbedDatasetReclustering(ToleranceCalculator):
             float: The calculated tolerance value.
         """
         self._ensure_perturbed_dataset()
+        if self.target_structures_threshold == "average":        
+            low_tolerance = self.binary_search_tolerance(
+                target_unique_vectors=self.target_structures,
+                find_largest_tolerance_for_target=False,
+            )
+            high_tolerance = self.binary_search_tolerance(
+                target_unique_vectors=self.target_structures,
+                find_largest_tolerance_for_target=True,
+            )
+            return (low_tolerance + high_tolerance) / 2
         
-        low_tolerance = self.binary_search_tolerance(
-            target_unique_vectors=self.target_structures,
-            find_largest_tolerance_for_target=False,
-        )
-        high_tolerance = self.binary_search_tolerance(
-            target_unique_vectors=self.target_structures,
-            find_largest_tolerance_for_target=True,
-        )
-        return (low_tolerance + high_tolerance) / 2
+        elif self.target_structures_threshold == "loose":
+            return self.binary_search_tolerance(
+                target_unique_vectors=self.target_structures,
+                find_largest_tolerance_for_target=True,
+            )
+        elif self.target_structures_threshold == "tight":
+            return self.binary_search_tolerance(
+                target_unique_vectors=self.target_structures,
+                find_largest_tolerance_for_target=False,
+            )
--- tests/unit/plugins/tolerance_calculators/test_perturbed_dataset_reclustering.py
+++ tests/unit/plugins/tolerance_calculators/test_perturbed_dataset_reclustering.py
@@ -37,11 +37,10 @@ def test_calculate_tolerance(request, dda_fixture):
     # generate rattled data and compute distance matrix
     tc.create_perturbed_dataset()
     tc.duplicate_detection_algorithm_object.pre_dda_processing(tc.tolerance_dataset_array)
-    print(tc.duplicate_detection_algorithm_object.distance_matrix.shape)
 
     # use these to calculate the tolerance
     tolerance = tc.calculate_tolerance()
-
+    print(f"Calculated Average Tolerance: {tolerance}")
     # ensure the tolerance is a positive float and that it yields the correct number of unique vectors in the original dataset when applied to the perturbed dataset
     assert isinstance(tolerance, float)
     assert tolerance > 0.0
@@ -56,6 +55,68 @@ def test_calculate_tolerance(request, dda_fixture):
         )
     assert unique_vectors == len(tc.duplicate_detection_algorithm_object.dataset_array)
 
+    # repeat for loose tolerance
+    tc_loose = PerturbedDatasetReclustering(
+        duplicate_detection_algorithm_object=dda,
+        perturbations_per_vector=100,
+        perturbation_scale=0.01,
+        binary_search_steps=20,
+        target_structures_threshold="loose",
+    )
+    tc_loose.create_perturbed_dataset()
+    tc_loose.duplicate_detection_algorithm_object.pre_dda_processing(tc_loose.tolerance_dataset_array)
+    
+
+    # use these to calculate the tolerance
+    loose_tolerance = tc_loose.calculate_tolerance()
+    print(f"Calculated Loose Tolerance: {loose_tolerance}")
+
+    assert isinstance(loose_tolerance, float)
+    assert loose_tolerance > 0.0
+    tc_loose.duplicate_detection_algorithm_object.tolerance = loose_tolerance
+    with tc_loose.temp_attr(
+        tc_loose.duplicate_detection_algorithm_object,
+        "dataset_array",
+        tc_loose.tolerance_dataset_array,
+    ):
+        unique_vectors = (
+            tc_loose.duplicate_detection_algorithm_object.get_dataset_unique_structures()
+        )
+    assert unique_vectors == len(tc_loose.duplicate_detection_algorithm_object.dataset_array)
+
+    assert loose_tolerance >= tolerance
+
+    # repeat for tight tolerance
+    tc_tight = PerturbedDatasetReclustering(
+        duplicate_detection_algorithm_object=dda,
+        perturbations_per_vector=100,
+        perturbation_scale=0.01,
+        binary_search_steps=20,
+        target_structures_threshold="tight",
+    )
+    tc_tight.create_perturbed_dataset()
+    tc_tight.duplicate_detection_algorithm_object.pre_dda_processing(tc_tight.tolerance_dataset_array)
+    
+
+    # use these to calculate the tolerance
+    tight_tolerance = tc_tight.calculate_tolerance()
+    print(f"Calculated Tight Tolerance: {tight_tolerance}")
+    assert isinstance(tight_tolerance, float)
+    assert tight_tolerance > 0.0
+    tc_tight.duplicate_detection_algorithm_object.tolerance = tight_tolerance
+    with tc_tight.temp_attr(
+        tc_tight.duplicate_detection_algorithm_object,
+        "dataset_array",
+        tc_tight.tolerance_dataset_array,
+    ):
+        unique_vectors = (
+            tc_tight.duplicate_detection_algorithm_object.get_dataset_unique_structures()
+        )
+    assert unique_vectors == len(tc_tight.duplicate_detection_algorithm_object.dataset_array)
+
+    assert tight_tolerance <= tolerance
+    assert tight_tolerance <= loose_tolerance
+
     tc.binary_search_steps = 1
     original_best_max = (
         np.ptp(tc.tolerance_dataset_array)
@@ -66,4 +127,5 @@ def test_calculate_tolerance(request, dda_fixture):
         match=f"No exact tolerance found for target of {len(dda.dataset_array)} unique vectors during binary search.\n Returning closest tolerance found: {original_best_max} with 1 unique vectors.",
     ):
         tolerance = tc.calculate_tolerance()
+
     # assert False
