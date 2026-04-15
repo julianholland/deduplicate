671ddaca01f9490f6fec587390c33c124bd42b28
Julian <holland@fhi.mpg.de>
2026-04-15 16:02:32 +0200

    fixed round then perturb bug, fixed reset input vector on hash array creation bug, added get_uniqueness_score to multihash

--- src/deduplicate_lib/plugins/duplicate_detection_algorithms/multi_hashing.py
+++ src/deduplicate_lib/plugins/duplicate_detection_algorithms/multi_hashing.py
@@ -10,11 +10,22 @@ from numba import njit
 
 @njit
 def fast_round_and_perturb(input_vector, perturbation_array, tolerance):
-    rounded_vector = np.round(input_vector / tolerance) * tolerance # pragma: no cover, tested but does not appear in coverage report due to numba jit compilation
-    round_and_perturb_array = np.zeros((perturbation_array.shape[0], len(input_vector))) # pragma: no cover
-    for i in range(perturbation_array.shape[0]): # pragma: no cover
-        round_and_perturb_array[i] = rounded_vector * perturbation_array[i] # pragma: no cover
-    return round_and_perturb_array # pragma: no cover
+    n_pert = perturbation_array.shape[0]
+    n_feat = input_vector.shape[0]
+    out = np.empty((n_pert, n_feat), dtype=input_vector.dtype)
+
+    for i in range(n_pert):
+        p = perturbation_array[i]
+        for j in range(n_feat):
+            v = p * input_vector[j]
+            out[i, j] = np.round(v / tolerance) * tolerance
+
+    return out 
+# def fast_round_and_perturb(input_vector, perturbation_array, tolerance):
+#     perturbed_vector_array = np.array([perturbation_array[i] * input_vector for i in range(perturbation_array.shape[0])])
+#     round_and_perturb_array = np.round(perturbed_vector_array / tolerance) * tolerance # pragma: no cover, tested but does not appear in coverage report due to numba jit compilation
+    
+#     return round_and_perturb_array # pragma: no cover
 
 @register_plugin(kind="duplicate_detection_algorithm", name="multi_hashing")
 class MultiHashing(DuplicateDetectionAlgorithm):
@@ -69,9 +80,11 @@ class MultiHashing(DuplicateDetectionAlgorithm):
         rounded_vector = np.round(self.input_vector / self.tolerance) * self.tolerance
         return rounded_vector
 
-    def create_hash_vector(self) -> np.ndarray:
+    def create_hash_vector(self, input_vector: np.ndarray | None = None) -> np.ndarray:
+        if input_vector is None:
+            input_vector = self.input_vector
         self._ensure_perturbation_array()
-        rounded_and_perturbed = fast_round_and_perturb(self.input_vector, self.perturbation_array, self.tolerance)
+        rounded_and_perturbed = fast_round_and_perturb(input_vector, self.perturbation_array, self.tolerance)
         hash_vector = np.zeros(self.perturbations, dtype=int)
         for i in range(self.perturbations):
             hash_vector[i] = hash(rounded_and_perturbed[i].tobytes())
@@ -88,8 +101,7 @@ class MultiHashing(DuplicateDetectionAlgorithm):
         )
         self._ensure_perturbation_array()
         for i in range(self.dataset_array.shape[0]):
-            self.input_vector = self.dataset_array[i]
-            hash_vector = self.create_hash_vector()
+            hash_vector = self.create_hash_vector(self.dataset_array[i])
             for j in range(self.perturbations):
                 if hash_vector[j] not in self.hash_vector_array[:, j]:
                     self.hash_vector_array[i, j] = hash_vector[j]
@@ -120,6 +132,17 @@ class MultiHashing(DuplicateDetectionAlgorithm):
         duplicate_structure = duplicate_vote_array >= self.acceptance_threshold
 
         return bool(duplicate_structure)
+    
+    def get_uniqueness_score(self) -> float:
+        self._ensure_hash_vector_array()
+        hash_vector = self.create_hash_vector()
+        clash_vector = np.zeros(self.perturbations, dtype=bool)
+        for hash_index, hash_value in enumerate(hash_vector):
+            if hash_value in self.hash_vector_array[:, hash_index]:
+                clash_vector[hash_index] = True
+        uniqueness_score = 1 - np.sum(clash_vector) / self.perturbations
+
+        return uniqueness_score
 
     def get_dataset_unique_structures(self) -> int:
         """Counts the number of unique structures in the dataset by checking for hash value clashes across the dataset for each perturbation.
